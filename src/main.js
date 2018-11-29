223ac899d3a97fca487f4e3722a2ef6344b0eb0b
天马行空 <464352933@qq.com>
2018-11-29 14:40:38 +0800

    vue init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// 重置样式
+import './assets/style/reset.css'
+// 解决一像素边框问题
+import './assets/style/border.css'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
